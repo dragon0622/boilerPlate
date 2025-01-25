ec4e972127d6e53758219844890cf908daaa6c8c
dragon0622 <iamdragon.dev@gmail.com>
2025-01-25 17:18:36 +0900

    feat : login 로직 구현

--- src/main/java/com/practice/bbas/controller/UserController.java
+++ src/main/java/com/practice/bbas/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.practice.bbas.controller;
 
+import com.practice.bbas.dto.AddUserRequestDTO;
+import com.practice.bbas.dto.AddUserResponseDTO;
+import com.practice.bbas.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/api/signup")
+    public AddUserResponseDTO signup(@RequestBody AddUserRequestDTO requestDTO){
+        return userService.saveUser(requestDTO);
+    }
+
+    @PostMapping("/api/signup")
+    public AddUserResponseDTO login(@RequestBody AddUserRequestDTO requestDTO){
+        return userService.saveUser(requestDTO);
+    }
 }
