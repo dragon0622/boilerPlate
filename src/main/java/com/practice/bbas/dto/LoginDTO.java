c27d180c9fc3a7b05bcb908f9e87277b180271bb
dragon0622 <iamdragon.dev@gmail.com>
2025-01-31 11:07:09 +0900

    feat : raw-form -> json

---
+++ src/main/java/com/practice/bbas/dto/LoginDTO.java
@@ -0,0 +1,12 @@
+package com.practice.bbas.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+
+    private String name;
+    private String password;
+}
