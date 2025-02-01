9c9ec8ed04ff610c18f8f0fd8882951fcbf192d7
dragon0622 <iamdragon.dev@gmail.com>
2025-02-01 14:09:52 +0900

    feat: comment mvc 생성

--- src/main/java/com/practice/bbas/service/CommentService.java
+++ src/main/java/com/practice/bbas/service/CommentService.java
@@ -1,7 +1,18 @@
 package com.practice.bbas.service;
 
+import com.practice.bbas.dto.CommentRequestDTO;
+import com.practice.bbas.dto.CommentResponseDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    public CommentResponseDTO addComment(CommentRequestDTO commentRequestDTO) {
+
+        return null;
+    }
+
+    public CommentResponseDTO deleteComment(CommentRequestDTO commentRequestDTO) {
+
+        return null;
+    }
 }
