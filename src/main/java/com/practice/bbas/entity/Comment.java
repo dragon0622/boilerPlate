91db1c2f097c02c2e98fdf318c00dde3231a7dc5
dragon0622 <iamdragon.dev@gmail.com>
2025-02-01 12:39:23 +0900

    feat: comment entity 생성

---
+++ src/main/java/com/practice/bbas/entity/Comment.java
@@ -0,0 +1,40 @@
+package com.practice.bbas.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Comment")
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @CreatedDate
+    @Column(name = "create_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at")
+    private LocalDateTime updatedAt;
+
+
+}
