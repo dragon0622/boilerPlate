e011e5e9f7d08c997e428c74c5a515e1ae217eee
dragon0622 <iamdragon.dev@gmail.com>
2025-02-01 12:39:48 +0900

    feat: post entity 생성

---
+++ src/main/java/com/practice/bbas/entity/Post.java
@@ -0,0 +1,42 @@
+package com.practice.bbas.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Post")
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Comment> comments = new ArrayList<>();
+
+    @CreatedDate
+    @Column(name = "create_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at")
+    private LocalDateTime updatedAt;
+
+
+}
